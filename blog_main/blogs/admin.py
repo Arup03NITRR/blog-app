fa23a4a20dbf9d853b8ca246b923a6873324487e
Arup <arup03.paul@gmail.com>
2025-02-28 13:08:31 +0530

    Updating Model Category

--- blog_main/blogs/admin.py
+++ blog_main/blogs/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Blogs
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
     list_display=['id', 'category_name', 'created_at', 'updated_at']
 
+class BlogsAdmin(admin.ModelAdmin):
+    list_display=['id', 'title']
+    prepopulated_fields={'slug':('title',)}
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Blogs, BlogsAdmin)
--- blog_main/blogs/models.py
+++ blog_main/blogs/models.py
@@ -9,6 +9,9 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural='Categories'
+    
+    def __str__(self):
+        return self.category_name
 
 STATUS_CHOICE=(
     ('draft', 'Drafted'),
@@ -27,3 +30,6 @@ class Blogs(models.Model):
     is_featured=models.BooleanField(default=False)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural='Blogs'
