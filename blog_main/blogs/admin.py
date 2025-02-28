f8b01b9ca2ae235855169f2babb9d546f6d81afe
Arup <arup03.paul@gmail.com>
2025-02-28 13:28:11 +0530

    Updating BlogAdmin

--- blog_main/blogs/admin.py
+++ blog_main/blogs/admin.py
@@ -6,7 +6,8 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display=['id', 'category_name', 'created_at', 'updated_at']
 
 class BlogsAdmin(admin.ModelAdmin):
-    list_display=['id', 'title']
+    list_display=['id', 'title', 'category', 'author', 'is_featured', 'created_at', 'updated_at', 'status']
     prepopulated_fields={'slug':('title',)}
+    list_editable=['is_featured',]
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Blogs, BlogsAdmin)
